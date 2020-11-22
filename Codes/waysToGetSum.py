f9d5468daa0826c5f64d8897c885b458656819a4
Ganesh Jadhav <ganeshjadhav221b@gmail.com>
2020-11-22 22:16:33 +0530

    Added logic of order in waysToGetSum

--- Codes/waysToGetSum.py
+++ Codes/waysToGetSum.py
@@ -1,12 +1,27 @@
-sum =  18
-numbers = [3,5,10]
+coins = [3,5,10]
+#coins = [1,3,5]
+n = len(coins)
+desiredSum = 15
 
-tempList = [0]*(sum+1)
-tempList[0] = 1
-temp = 0
-for i in numbers:
-    for j in range(i,sum+1):
-        if j%i==0 or tempList[j-i] >0:
-            tempList[j]+=1
-        #print(i,j,tempList)
-print(max(tempList))
+dp = [0]*(desiredSum+1)
+dp[0] = 1
+
+def orderDoesntMatter():
+    for i in range(1,len(dp)):
+        for coin in coins:
+            if i-coin>=0:
+                dp[i] += dp[i-coin]
+    print(dp[1:])
+    
+def orderMatters():
+    for coin in coins:
+        for i in range(coin,len(dp)):    
+            if i%coin==0 or dp[i-coin]>0:
+                if(i==8):
+                    print(coins)
+                dp[i] += 1
+    print(dp[1:])
+
+orderMatters()
+    
+    
